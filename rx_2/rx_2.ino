83e9b2152207905ecf1fbf870ad56d5e8da0ef4a
beacampo <beatrice.campo@epfl.ch>
2025-04-06 11:28:57 +0200

    Testing flagged communication between 3 seeeduinos

--- rx_2/rx_2.ino
+++ rx_2/rx_2.ino
@@ -7,26 +7,26 @@
 #include <RF24.h>
 
 struct Payload {
-  uint8_t commandFlags; // unsigned integer of 8 bits, used for flags
+  uint8_t commandFlags;
   int Data;
 };
 
 Payload receivedData;
-const int deviceID = 1; // device ID
+const int deviceID = 2; // device ID
 
-RF24 wireless(10, 9); // Set accordingly to branching
+RF24 wireless(7, 8); // Set accordingly to branching
 const byte addresses[][6] = {"00001","00002"};
- 
+
 void setup() {
   Serial.begin(9600);
   wireless.begin();
   wireless.setChannel(125);
-  wireless.openWritingPipe(addresses[1])
-  wireless.openReadingPipe(1, addresses[0]);
+  wireless.openWritingPipe(addresses[1]);
+  wireless.openReadingPipe(1, addresses[0]); // Address of transmitter and data pipe 
   wireless.setPALevel(RF24_PA_LOW);
   wireless.startListening();
 }
- 
+
 void loop() {
   if (wireless.available()) {
     wireless.read(&receivedData, sizeof(receiveData));
--- tx/tx.ino
+++ tx/tx.ino
@@ -6,25 +6,43 @@
 #include <nRF24L01.h>
 #include <RF24.h>
  
+struct Payload {
+  uint8_t commandFlags;
+  int Data;
+};
+
+
 RF24 wireless(10, 9);
-const byte address[6] = "00001";
-const char msg[] = "Salut Oceane";
+const byte addresses[][6] = {"00001", "00002","00003"};
+
+Payload datatoSend;
+const int deviceID = 0;
+
  
 void setup() {
   Serial.begin(9600);
   wireless.begin();
   wireless.setChannel(125);
-  wireless.openWritingPipe(address);
+  wireless.openWritingPipe(addresses[0]); // Address of Transmitter which will send data
+  wireless.openReadingPipe(1, addresses[1]);
   wireless.setPALevel(RF24_PA_LOW);
   wireless.stopListening();
 }
  
+ 
 void loop() {
-  bool result = wireless.write(&msg, sizeof(msg));
-  if (result) {
-    Serial.println("Comunicazione inviata correttamente");
-  } else {
-    Serial.println("Errore: messaggio non ricevuto");
-  }
-  delay(1000);
+  datatoSend.commandFlags = B00000100; // the slave1 will not respond but slave2 yes
+  datatoSend.Data = random(0,100);
+
+  Serial.print("Sending: ");
+  Serial.print("Flags = ");
+  Serial.print(dataToSend.commandFlags, BIN);
+  Serial.print(", Data = ");
+  Serial.println(dataToSend.Data);
+  wireless.write(&datatoSend, sizeof(datatoSend));
+
+  radio.write(&dataToSend, sizeof(dataToSend));
+
+  delay(2000); // Wait 2 seconds before sending again
+  
 }
