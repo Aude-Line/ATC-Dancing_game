1609dfa0d9728fb6f876a2081564d46eb977b26f
Aude-Line <aude-line.fleury@epfl.ch>
2025-04-13 14:51:07 +0200

    tests

--- src/master/main.cpp
+++ src/master/main.cpp
@@ -1,15 +1,10 @@
 #include <Arduino.h>
 
-#ifndef SLAVE_ID
-#define SLAVE_ID 0
-#endif
-
 void setup() {
   Serial.begin(9600);
-  Serial.print("Je suis le slave ID : ");
-  Serial.println(SLAVE_ID);
+  Serial.println("Je suis le master !");
 }
 
 void loop() {
-  // comportement selon SLAVE_ID
+  // communication avec les slaves
 }
---
+++ src/slave/main.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+
+#ifndef SLAVE_ID
+#define SLAVE_ID -1
+#endif
+
+void setup() {
+  Serial.begin(9600);
+  Serial.print("Je suis le slave ID : ");
+  Serial.println(SLAVE_ID);
+}
+
+void loop() {
+  // comportement selon SLAVE_ID
+}
