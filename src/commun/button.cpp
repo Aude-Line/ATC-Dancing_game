fe7a8d93fe92637675d308161b1e65f723d583c0
Aude-Line <aude-line.fleury@epfl.ch>
2025-04-15 17:43:27 +0200

    modif valeur, car en courant on est en analog

--- src/commun/button.cpp
+++ src/commun/button.cpp
@@ -9,7 +9,7 @@ void Button::init() {
   pinMode(buttonPin, INPUT_PULLUP);
   if (ledAw9523) {
     aw->pinMode(ledPin, AW9523_LED_MODE); // set to constant current drive!
-    aw->digitalWrite(ledPin, HIGH);     // LED éteinte (active LOW)
+    aw->analogWrite(ledPin, 0);     // LED éteinte (active LOW)
   } else {
     pinMode(ledPin, OUTPUT);
     digitalWrite(ledPin, LOW);          // LED éteinte (active HIGH)
@@ -31,7 +31,7 @@ bool Button::isPressed(){ // detection uniquement d'une falling edge
 
 void Button::turnOnLed(){
   if (ledAw9523) {
-    aw->digitalWrite(ledPin, LOW); // LED allumée (active LOW)
+    aw->analogWrite(ledPin, 255); // LED allumée (active LOW)
   } else {
     digitalWrite(ledPin, HIGH);    // LED allumée (active HIGH)
   }
@@ -40,7 +40,7 @@ void Button::turnOnLed(){
 
 void Button::turnOffLed(){
   if (ledAw9523) {
-    aw->digitalWrite(ledPin, HIGH); // LED éteinte (active LOW)
+    aw->analogWrite(ledPin, 0); // LED éteinte (active LOW)
   } else {
     digitalWrite(ledPin, LOW);      // LED éteinte (active HIGH)
   }
